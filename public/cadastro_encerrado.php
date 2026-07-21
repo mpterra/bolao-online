c23dad286e323510bd72cb57f0abf75522ef6384
mpterra <mauriciopterra@gmail.com>
2026-07-21 15:05:24 -0300

    Botoes removidos

--- public/cadastro_encerrado.php
+++ public/cadastro_encerrado.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . "/php/security.php";
 require_once dirname(__DIR__) . "/php/registration_lock.php";
-require_once __DIR__ . "/partials/whatsapp_float.php";
 app_start_session();
 app_send_security_headers();
 ?>
@@ -40,6 +39,5 @@ app_send_security_headers();
             </div>
         </main>
     </div>
-    <?php render_whatsapp_float(); ?>
 </body>
 </html>
